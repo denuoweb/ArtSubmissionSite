34a5a2b5eb56a0434e37af84ced3785ad79ed41c
denuoweb <jaron@rosenau.info>
2025-01-01 18:55:23 -0800

    add debug and port to config file

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 from app import app
 
+import toml
+
+# Load config
+config = toml.load("config.toml")
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(
+        debug=config["flask"]["DEBUG"],
+        host=config["flask"]["HOST"],
+        port=config["flask"]["PORT"]
+    )
